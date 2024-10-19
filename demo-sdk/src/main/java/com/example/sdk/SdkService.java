aa0fcdbb53522b1f5160068405e1a4673f5b30e1
Amulya Reddy <amulyareddyk97@gmail.com>
2024-10-19 16:31:12 +0530

    common modules sdk

---
+++ demo-sdk/src/main/java/com/example/sdk/SdkService.java
@@ -0,0 +1,10 @@
+package com.example.sdk;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class SdkService {
+	public void process(){
+		System.out.println("processing in SDK");
+	}
+}
